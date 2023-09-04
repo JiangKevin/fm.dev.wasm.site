3063e124d7dec062b3a7868244e22903b53da23a
JiangKevin <JiangKevin@users.noreply.github.com>
2023-09-04 21:07:18 +0800

    清空

--- vue.vuetify.wasm.site/zshshell/build_and_copy_tosite.sh
+++
@@ -1,8 +0,0 @@
-#
-clear
-reset
-# 
-cp -rf ./static/*  /Users/kevin.jiang/Dev/src/website/web/staic
-cnpm run build
-cp -rf ./dist/* /Users/kevin.jiang/Dev/src/website/web/
-cp -rf ./static/*  /Users/kevin.jiang/Dev/src/website/web/
